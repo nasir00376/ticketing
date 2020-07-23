993eb6a89165cad8989f2c4674e5742f1a992583
Nasir Abbas <nasir000376@gmail.com>
2020-07-23 21:18:27 +0500

    feat: Initialize auth service with express

---
+++ auth/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+import { json } from 'body-parser';
+
+const PORT = 3000;
+
+const app = express();
+
+// Middlewares
+app.use(json());
+
+app.listen(PORT, () => console.log(`Auth service is listening on port: ${PORT}`));
